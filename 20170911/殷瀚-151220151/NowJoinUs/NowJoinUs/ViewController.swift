de8d138a89c1c83558b777effd6f096363c2905f
AlexYinHan <411629507@qq.com>
2017-09-21 14:51:52 +0800

    delete .DS_store

---
+++ 20170911/殷瀚-151220151/NowJoinUs/NowJoinUs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NowJoinUs
+//
+//  Created by Apple on 2017/9/20.
+//  Copyright © 2017年 NJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func exitMyApp(){
+        exit(0)
+    }
+}
+
