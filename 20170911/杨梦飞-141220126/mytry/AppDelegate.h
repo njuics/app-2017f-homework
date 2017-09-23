501727d0922d4eb0fb9c40b6c30cae7afe4dc777
[yangmengfei] <[ymf321@qq.com]>
2017-09-23 23:56:08 +0800

    Finish helloworld!

---
+++ 20170911/杨梦飞-141220126/mytry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytry
+//
+//  Created by apple on 2017/9/10.
+//  Copyright © 2017年 nju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
